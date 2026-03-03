7bfe32243bfc05bbbd83811f5753337ae6c5f798
mwrqxesa <toconfusaqueroumtempo@gmail.com>
2026-03-03 13:51:48 -0300

    Update serverinfo.js

--- commands/util/serverinfo.js
+++ commands/util/serverinfo.js
@@ -1,1 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('serverinfo')
+    .setDescription('Mostra informações do servidor'),
+
+  async execute(interaction) {
+    const guild = interaction.guild;
+
+    const embed = new EmbedBuilder()
+      .setTitle(`🏠 ${guild.name}`)
+      .setThumbnail(guild.iconURL({ size: 256 }))
+      .addFields(
+        { name: '🆔 ID', value: guild.id, inline: false },
+        { name: '👥 Membros', value: String(guild.memberCount), inline: true },
+        { name: '📅 Criado em', value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>`, inline: false }
+      )
+      .setFooter({ text: 'Zangwdo • servidor' });
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
