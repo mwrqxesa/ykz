b5ff14453cc3aa0d8c0df1f60a1f638660374dd5
mwrqxesa <toconfusaqueroumtempo@gmail.com>
2026-03-03 13:50:20 -0300

    Update ping.js

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -1,1 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Mostra a latência do bot'),
+
+  async execute(interaction, client) {
+    const msg = await interaction.reply({
+      content: '🏓 Calculando...',
+      fetchReply: true
+    });
+
+    const latency = msg.createdTimestamp - interaction.createdTimestamp;
+    const apiPing = Math.round(client.ws.ping);
+
+    await interaction.editReply(
+      `🏓 Pong!\n📡 Latência: **${latency}ms**\n🌐 API: **${apiPing}ms**`
+    );
+  }
+};
