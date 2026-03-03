ddeedfcf64de483e8ee1f72ca7ce984c736c5c91
mwrqxesa <toconfusaqueroumtempo@gmail.com>
2026-03-03 13:51:25 -0300

    Update userinfo.js

--- commands/util/userinfo.js
+++ commands/util/userinfo.js
@@ -1,1 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('userinfo')
+    .setDescription('Mostra informações de um usuário')
+    .addUserOption(option =>
+      option.setName('usuario')
+        .setDescription('Usuário alvo')
+        .setRequired(false)
+    ),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser('usuario') || interaction.user;
+    const member = await interaction.guild.members.fetch(user.id).catch(() => null);
+
+    const embed = new EmbedBuilder()
+      .setTitle(`👤 Informações de ${user.username}`)
+      .setThumbnail(user.displayAvatarURL({ size: 256 }))
+      .addFields(
+        { name: '🆔 ID', value: user.id, inline: false },
+        { name: '📅 Conta criada', value: `<t:${Math.floor(user.createdTimestamp / 1000)}:F>`, inline: false },
+        { name: '📥 Entrou no servidor', value: member?.joinedTimestamp ? `<t:${Math.floor(member.joinedTimestamp / 1000)}:F>` : 'Não encontrado', inline: false }
+      );
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
