2f147aeb31a3e5d2fcb48939b402e0c794ce4a97
mwrqxesa <toconfusaqueroumtempo@gmail.com>
2026-03-03 13:50:42 -0300

    Update avatar.js

--- commands/util/avatar.js
+++ commands/util/avatar.js
@@ -1,1 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('avatar')
+    .setDescription('Mostra o avatar de um usuário')
+    .addUserOption(option =>
+      option.setName('usuario')
+        .setDescription('Usuário alvo')
+        .setRequired(false)
+    ),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser('usuario') || interaction.user;
+
+    const embed = new EmbedBuilder()
+      .setTitle(`🖼️ Avatar de ${user.username}`)
+      .setImage(user.displayAvatarURL({ size: 1024, extension: 'png' }))
+      .setFooter({ text: 'Zangwdo • utilidades' });
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
